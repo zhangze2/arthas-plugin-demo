c7a0edc55bf86aeb9c0bb1700137ab45f5e721be
zhangze <zhangze.zz@qq.com>
2022-05-30 12:39:55 +0800

    Add：Scheduled 注解注释

--- src/main/java/com/wangji92/arthas/plugin/demo/controller/LoggerDemo.java
+++ src/main/java/com/wangji92/arthas/plugin/demo/controller/LoggerDemo.java
@@ -5,7 +5,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 /**
- * 体验一下修改logger的等级
+ * 观察用 logger 命令修改 logger 的等级效果
  *
  * @author 汪小哥
  * @date 18-04-2020
@@ -15,9 +15,11 @@ import org.springframework.stereotype.Component;
 public class LoggerDemo {
 
     /**
+     *
      * logger --name com.wangji92.arthas.plugin.demo.controller.LoggerDemo --level trace -c 18b4aac2
+     * 第一次延迟1秒后执行，之后按fixedRate的规则每5秒执行一次
      */
-    @Scheduled(initialDelay = 1000, fixedRate = 5000)
+    @Scheduled(initialDelay=1000, fixedRate=5000)
     public void sample() {
         log.trace("....trace");
         log.debug("......debug");
